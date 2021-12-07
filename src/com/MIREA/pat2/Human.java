8b9ebf3d4c4d04e2f0f5b71b1e2cd27387b2110d
cheshrst <R59rrzdj!>
2021-12-08 00:43:53 +0300

    aa

---
+++ src/com/MIREA/pat2/Human.java
@@ -0,0 +1,8 @@
+package com.MIREA.pat2;
+
+public class Human {
+
+
+    public void calculate(){};
+    public void sayHello(){};
+}
---
+++ src/com/MIREA/pat2/Main.java
@@ -0,0 +1,15 @@
+package com.MIREA.pat2;
+
+import com.MIREA.pat.Human;
+import com.MIREA.pat.Oleg;
+import com.MIREA.pat.Superman;
+
+public class Main {
+    public static void main(String[] args) {
+    Human oleg = new Oleg();
+
+    Human superm = new Superman();
+    oleg.sayHello();
+
+    }
+}
